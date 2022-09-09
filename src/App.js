92df11e28b295f8d83e1e6c566adaff0eeb05cc1
Shubham Anchliya <info.shubhamanchliya@gmail.com>
2022-09-09 15:37:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+// import React, { useEffect } from "react";
+// import "./style.css";
+// import { useFormik } from "formik";
+// import Select from "react-select";
+// import csc from "country-state-city";
 
-function App() {
+// export default function App() {
+//   const addressFromik = useFormik({
+//     initialValues: {
+//       country: "India",
+//       state: null,
+//       city: null
+//     },
+//     onSubmit: (values) => console.log(JSON.stringify(values))
+//   });
+
+//   const countries = csc.getAllCountries();
+
+//   const updatedCountries = countries.map((country) => ({
+//     label: country.name,
+//     value: country.id,
+//     ...country
+//   }));
+//   const updatedStates = (countryId) =>
+//     csc
+//       .getStatesOfCountry(countryId)
+//       .map((state) => ({ label: state.name, value: state.id, ...state }));
+//   const updatedCities = (stateId) =>
+//     csc
+//       .getCitiesOfState(stateId)
+//       .map((city) => ({ label: city.name, value: city.id, ...city }));
+
+//   const { values, handleSubmit, setFieldValue, setValues } = addressFromik;
+
+//   useEffect(() => {}, [values]);
+
+//   return (
+//     <div className="App">
+//       <form onSubmit={handleSubmit}>
+//         <Select
+//           id="country"
+//           name="country"
+//           label="country"
+//           className="selectTag"
+//           options={updatedCountries}
+//           value={values.country}
+//           // onChange={value => {
+//           //   setFieldValue("country", value);
+//           //   setFieldValue("state", null);
+//           //   setFieldValue("city", null);
+//           // }}
+//           onChange={(value) => {
+//             setValues({ country: value, state: null, city: null }, false);
+//           }}
+//         />
+//         <Select
+//           id="state"
+//           name="state"
+//           className="selectTag"
+//           options={updatedStates(values.country ? values.country.value : null)}
+//           value={values.state}
+//           onChange={(value) => {
+//             setValues({ state: value, city: null }, false);
+//           }}
+//         />
+//         <Select
+//           id="city"
+//           name="city"
+//           className="selectTag"
+//           options={updatedCities(values.state ? values.state.value : null)}
+//           value={values.city}
+//           onChange={(value) => setFieldValue("city", value)}
+//         />
+//         <button className="btnS" type="submit">Submit</button>
+//         <p>{JSON.stringify(csc.get)}</p>
+//       </form>
+//     </div>
+//   );
+// }
+
+
+import React from 'react'
+import CountryState from './CountryCity'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <CountryState />
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/CountryCity.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect } from "react";
+import "./style.css";
+import { useFormik } from "formik";
+import Select from "react-select";
+import csc from "country-state-city";
+
+export default function CountryState() {
+  const addressFromik = useFormik({
+    initialValues: {
+      country: "India",
+      state: null,
+      city: null
+    },
+    onSubmit: (values) => console.log(JSON.stringify(values))
+  });
+
+  const countries = csc.getAllCountries();
+
+  const updatedCountries = countries.map((country) => ({
+    label: country.name,
+    value: country.id,
+    ...country
+  }));
+  const updatedStates = (countryId) =>
+
+    csc
+      .getStatesOfCountry(countryId)
+      .map((state) => ({ label: state.name, value: state.id, ...state }));
+      
+  const updatedCities = (stateId) =>
+    csc
+      .getCitiesOfState(stateId)
+      .map((city) => ({ label: city.name, value: city.id, ...city }));
+
+  const { values, handleSubmit, setFieldValue, setValues } = addressFromik;
+
+  useEffect(() => {}, [values]);
+
+  return (
+    <div className="App">
+      <form onSubmit={handleSubmit}>
+        <Select
+          id="country"
+          name="country"
+          label="country"
+          className="selectTag"
+          options={updatedCountries}
+          value={values.country}
+          // onChange={value => {
+          //   setFieldValue("country", value);
+          //   setFieldValue("state", null);
+          //   setFieldValue("city", null);
+          // }}
+          onChange={(value) => {
+            setValues({ country: value, state: null, city: null }, false);
+          }}
+        />
+        <Select
+          id="state"
+          name="state"
+          className="selectTag"
+          options={updatedStates(values.country ? values.country.value : null)}
+          value={values.state}
+          onChange={(value) => {
+            setValues({ state: value, city: null }, false);
+          }}
+        />
+        <Select
+          id="city"
+          name="city"
+          className="selectTag"
+          options={updatedCities(values.state ? values.state.value : null)}
+          value={values.city}
+          onChange={(value) => setFieldValue("city", value)}
+        />
+        <button className="btnS" type="submit">Submit</button>
+        {/* <p>{JSON.stringify(csc.get)}</p> */}
+      </form>
+    </div>
+  );
+}
